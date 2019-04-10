510320ca7eb1239c93f73526aceb72c258911400
Carlo Sciolla <carlo.sciolla@gmail.com>
2019-04-10 21:18:41 +0200

    Kinda manually tested

--- src/clj/addressbookje/handler.clj
+++ src/clj/addressbookje/handler.clj
@@ -1,31 +1,35 @@
 (ns addressbookje.handler
   (:require [compojure.core :refer [GET POST defroutes routes]]
             [compojure.route :refer [resources]]
-            [ring.util.response :refer [resource-response response]]
+            [ring.util.response :refer [resource-response response status]]
             [ring.middleware.reload :refer [wrap-reload]]
-            [ring.middleware.json :refer [wrap-json-response]]))
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]))
 
 (defonce contacts (atom [{:name "Carlo Sciolla"
                           :email "carlo.sciolla@gmail.com"}]))
 
 (defn add-contact
   "Adds a new contact to the in-memory db"
-  [contacts name email]
+  [contacts {:strs [name email]}]
   (swap! contacts conj {:name name
-                        :email email}))
+                        :email email})
+  (-> (response "Created")
+      (status 201)))
 
 (defroutes api-routes
   (GET "/api/contacts"  []
     (response @contacts))
-  (POST "/api/contacts" [name email]
-    (add-contact contacts name email)))
+  (POST "/api/contacts" {body :body}
+    (add-contact contacts body)))
 
 (defroutes static-routes
   (GET "/" [] (resource-response "index.html" {:root "public"}))
   (resources "/"))
 
 (def app (routes static-routes
-                 (wrap-json-response api-routes)))
+                 (-> api-routes
+                     wrap-json-response
+                     wrap-json-body)))
 
 (def dev-handler (-> #'app wrap-reload))
 
