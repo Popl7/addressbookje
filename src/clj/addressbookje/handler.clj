28928d7c765db33f0834ed67b1c5ca2a7aeeb900
Carlo Sciolla <carlo.sciolla@gmail.com>
2019-04-10 21:06:51 +0200

    Added add-contacts API on the backend

--- src/clj/addressbookje/handler.clj
+++ src/clj/addressbookje/handler.clj
@@ -1,12 +1,24 @@
 (ns addressbookje.handler
-  (:require [compojure.core :refer [GET defroutes routes]]
+  (:require [compojure.core :refer [GET POST defroutes routes]]
             [compojure.route :refer [resources]]
             [ring.util.response :refer [resource-response response]]
             [ring.middleware.reload :refer [wrap-reload]]
             [ring.middleware.json :refer [wrap-json-response]]))
 
+(defonce contacts (atom [{:name "Carlo Sciolla"
+                          :email "carlo.sciolla@gmail.com"}]))
+
+(defn add-contact
+  "Adds a new contact to the in-memory db"
+  [contacts name email]
+  (swap! contacts conj {:name name
+                        :email email}))
+
 (defroutes api-routes
-  (GET "/api/contacts" [] (response "test")))
+  (GET "/api/contacts"  []
+    (response @contacts))
+  (POST "/api/contacts" [name email]
+    (add-contact contacts name email)))
 
 (defroutes static-routes
   (GET "/" [] (resource-response "index.html" {:root "public"}))
